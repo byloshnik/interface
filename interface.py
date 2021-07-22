8c59e4e4f103d32bfc64d1581870ac9f24c3e4a8
VanSlam8 <van.slam@mail.ru>
2021-07-22 17:14:15 +0300

    inteface.py

--- interface.py
+++ interface.py
@@ -1,12 +1,11 @@
 import socket
-
+import time
 from flask import Flask, render_template, url_for, jsonify
 import sys
 import linuxcnc
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-print(f'Local IP: {local_ip}')
+local_ip = socket.gethostbyname(socket.getfqdn())
+print('Local IP: '+ local_ip)
 
 app = Flask(__name__)
 
@@ -27,7 +26,9 @@ def api_all():
 
 @app.route('/move_left')
 def move_left():
-    c.jog(linuxcnc.JOG_INCREMENT, 0, 3000, -10)
+    s.poll()
+    print('Status 444: ' + str(s.state))
+    c.jog(linuxcnc.JOG_INCREMENT, 0, 10, 250)
     lastAction['id'] += 1
     lastAction['last_action'] = 'move_left'
     return render_template("index_back.html")
@@ -41,5 +42,4 @@ def move_right():
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=12)
